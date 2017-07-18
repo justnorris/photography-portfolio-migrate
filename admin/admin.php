e6cd2d5c7b0d976dc265e58cabf39f06867ac485
Norris <im@justnorris.com>
2017-07-18 13:55:43 +0300

    Migration instructions

--- admin/admin.php
+++ admin/admin.php
@@ -56,21 +56,46 @@ class Settings_Page {
 		<div class="wrap">
 			<h1>Easy Photography Portfolio: Migrate</h1>
 
-			<br>
+			<div class="eppmig-migration-instructions">
+
+				<h2>Backup your database!</h2>
+				<p>
+					Before you start the migration, it's highly recommended that you <b>backup your database first!</b><br>
+					Don't worry, it's highly unlikely that something will go wrong, but it's better to be safe than sorry!<br>
+
+					<br>
+					Install a plugin like <a href="https://wordpress.org/plugins/backwpup/">BackWPup</a> and backup your database.
+					Here is a complete <a href="http://go.colormelon.com/database-backup-tutorial">video tutorial
+						guide</a> how to use the plugin.
+
+					<br>
+					Note, that you <b>don't have to backup files</b> - this migration isn't going to alter the files in any way, it's
+					going to convert only the database entries to Easy Photography Portfolio format.
+
+				</p>
+
+			</div>
 
 			<div class="eppmig-requirements">
-				<h2>Requirements</h2>
 
+				<h2>Checking migration requirements...</h2>
 				<?php $this->requirements->show_requirements(); ?>
 
 
 				<?php if ( $this->requirements->all_requirements_met() ): ?>
+
+					<div class="eppmig-requirements__ready">
+						<p>
+							Ready to migrate when you are. Click the button below to begin migrating your posts!
+						</p>
+					</div>
+
 					<form method="post" action="tools.php?page=photography-portfolio-migrate">
 						<?php wp_nonce_field( 'migrate_epp' ); ?>
-						<?php submit_button( 'Migrate' ); ?>
+						<?php submit_button( 'Migrate to Easy Photography Portfolio' ); ?>
 					</form>
 				<?php else: ?>
-					<a class="button-primary disabled">Migrate</a>
+					<a class="button-primary button-large disabled">Migrate to Easy Photography Portfolio</a>
 				<?php endif; ?>
 			</div> <!-- .eppmig-requirements -->
 
