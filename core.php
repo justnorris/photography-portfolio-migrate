4e3e90d5ae65f8a983b20a4d0b59d4e4f50e39db
Norris <im@justnorris.com>
2017-07-19 13:22:04 +0300

    Clean up core, remove redundtant is_admin()

--- core.php
+++ core.php
@@ -5,6 +5,7 @@ use Phormig\Admin\Settings_Page;
 use Phormig\Migrate\Migrate;
 use Phormig\Migrate\Migration_Requirements;
 use Phormig\Migrate\Requirements\Categories_To_migrate;
+use Phormig\Migrate\Requirements\EPP_Active;
 use Phormig\Migrate\Requirements\Posts_To_Migrate;
 
 
@@ -20,57 +21,56 @@ $settings = apply_filters(
 	]
 );
 
-if ( is_admin() ) {
 
-
-	require_once PHORMIG_ABSPATH . 'admin/admin.php';
+require_once PHORMIG_ABSPATH . 'admin/admin.php';
 
 
 // Include Requirements
-	require_once PHORMIG_ABSPATH . 'migrate/Migrate.php';
-	require_once PHORMIG_ABSPATH . 'migrate/Migration_Requirements.php';
-	require_once PHORMIG_ABSPATH . 'migrate/requirements/Requirement.php';
-	require_once PHORMIG_ABSPATH . 'migrate/requirements/Posts_To_Migrate.php';
-	require_once PHORMIG_ABSPATH . 'migrate/requirements/Categories_To_Migrate.php';
+require_once PHORMIG_ABSPATH . 'migrate/Migrate.php';
+require_once PHORMIG_ABSPATH . 'migrate/Migration_Requirements.php';
+require_once PHORMIG_ABSPATH . 'migrate/requirements/Requirement.php';
+require_once PHORMIG_ABSPATH . 'migrate/requirements/EPP_Active.php';
+require_once PHORMIG_ABSPATH . 'migrate/requirements/Posts_To_Migrate.php';
+require_once PHORMIG_ABSPATH . 'migrate/requirements/Categories_To_Migrate.php';
 
-	/**
-	 * Check the requirements
-	 */
-	$requirements = new Migration_Requirements(
-		[
-			new Posts_To_Migrate( $settings['post_type'] ),
-			new Categories_To_migrate( $settings['taxonomy'] ),
+/**
+ * Check the requirements
+ */
+$requirements = new Migration_Requirements(
+	[
+		new EPP_Active(),
+		new Posts_To_Migrate( $settings['post_type'] ),
+		new Categories_To_migrate( $settings['taxonomy'] ),
 
-		]
-	);
+	]
+);
 
-	/**
-	 * Create migration instance
-	 */
-	$migration = new Migrate( $settings );
-
-
-	/*
-	 * Check for $_POST Requests
-	 */
-	if ( // Post request is set
-		! empty( $_POST )
-
-		// Has the correct admin referer nonce
-		&& check_admin_referer( 'migrate_epp' )
-
-		// Not doing ajax
-		&& false === ( defined( 'DOING_AJAX' ) && DOING_AJAX )
-
-		// All the configuration requirements are met
-		&& $requirements->all_requirements_met()
-	) {
-		$migration->migrate();
-	}
+/**
+ * Create migration instance
+ */
+$migration = new Migrate( $settings );
 
 
-	/**
-	 * Create a settings page
-	 */
-	$settings_page = new Settings_Page( $requirements, $migration );
+/*
+ * Check for $_POST Requests
+ */
+if ( // Post request is set
+	! empty( $_POST )
+
+	// Has the correct admin referer nonce
+	&& check_admin_referer( 'migrate_epp' )
+
+	// Not doing ajax
+	&& false === ( defined( 'DOING_AJAX' ) && DOING_AJAX )
+
+	// All the configuration requirements are met
+	&& $requirements->all_requirements_met()
+) {
+	$migration->migrate();
 }
+
+
+/**
+ * Create a settings page
+ */
+$settings_page = new Settings_Page( $requirements, $migration );
--- photography-portfolio-migrate.php
+++ photography-portfolio-migrate.php
@@ -28,44 +28,60 @@ define( 'PHORMIG_ABSPATH', $__DIR . '/' );
 define( 'PHORMIG_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'PHORMIG_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 
+function phortmig_auto_deactivate() {
+
+	include( ABSPATH . "wp-includes/pluggable.php" );
+
+	if ( current_user_can( 'activate_plugins' ) ) {
+
+		// Deactivate Plugin
+		add_action(
+			'admin_init',
+			function () {
+
+				deactivate_plugins( plugin_basename( __FILE__ ) );
+			}
+		);
+
+		// Show message
+		add_action(
+			'admin_notices',
+			function () {
+
+				echo '<div class="error">';
+				echo wp_kses_post(
+					__(
+						'<p>You have to activate "Easy Photography Portfolio" before activating "Easy Photography Portfolio: Migrate" plugin. Please try again.</p>'
+						,
+						'photography-portfolio-migrate'
+					)
+				);
+				echo '</div>';
+			}
+		);
+
+	}
+
+
+}
 
 function phormig_initialize() {
 
+	if ( ! is_admin() ) {
+		return;
+	}
+
 	if ( ! class_exists( 'Colormelon_Photography_Portfolio' ) ) {
+		phortmig_auto_deactivate();
 
-		include( ABSPATH . "wp-includes/pluggable.php" );
-
-		function phortmig_auto_deactivate() {
-
-			deactivate_plugins( plugin_basename( __FILE__ ) );
-		}
-
-		if ( current_user_can( 'activate_plugins' ) ) {
-			add_action( 'admin_init', 'phortmig_auto_deactivate' );
-			add_action(
-				'admin_notices',
-				function () {
-
-					echo '<div class="error">';
-					echo wp_kses_post(
-						__(
-							'<p>You have to activate "Easy Photography Portfolio" before activating "Easy Photography Portfolio: Migrate" plugin. Please try again.</p>'
-							,
-							'photography-portfolio-migrate'
-						)
-					);
-					echo '</div>';
-				}
-			);
-
-		}
+		return;
 	}
 
 
-	else {
-		require_once PHORMIG_ABSPATH . 'core.php';
-	}
+	// Initialize Core
+	require_once PHORMIG_ABSPATH . 'core.php';
+
 }
 
 
-add_action( 'after_setup_theme', 'phormig_initialize', 250 );
+add_action( 'init', 'phormig_initialize' );
