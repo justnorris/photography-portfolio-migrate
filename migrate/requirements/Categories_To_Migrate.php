f6f9da7849b9e7e43b5ea8ecd16ea6cca0d68639
Norris <im@justnorris.com>
2017-07-19 13:22:29 +0300

    Improve category require validation

--- migrate/requirements/Categories_To_Migrate.php
+++ migrate/requirements/Categories_To_Migrate.php
@@ -8,7 +8,7 @@ class Categories_To_migrate extends Requirement {
 	/**
 	 * @var
 	 */
-	private $category_slug;
+	private $taxonomy;
 
 
 	/**
@@ -17,26 +17,26 @@ class Categories_To_migrate extends Requirement {
 	public function __construct( $category_slug ) {
 
 		parent::__construct();
-		$this->category_slug = $category_slug;
+		$this->taxonomy = $category_slug;
 	}
 
 
 	public function instructions() {
 
-		$dynamic_url = admin_url( 'edit.php?taxonomy=' . $this->category_slug );
+		$dynamic_url = admin_url( 'edit.php?taxonomy=' . $this->taxonomy );
 		$epp_url     = admin_url( 'edit.php?post_type=phort_post&taxonomy=phort_post_category' );
 
 		return "
 		<div class='phormig-condition-unmet'>
 			<p>
 			<b>If you're not using portfolio categories</b> - you can ignore this warning!
-			Looks like there are no portfolio categories to migrate in taxonomy <b>{$this->category_slug}</b> <br>
+			Looks like there are no portfolio categories to migrate in taxonomy <b>{$this->taxonomy}</b> <br>
 			</p>
 			
 			<ol>
 				<li>Make sure that the old version of the portfolio plugin is still active while migrating
 				<li>Have you already migrated the portfolio? Check Easy Photography Portfolio posts: <a target='_blank' href='$epp_url'>{$epp_url}</a>
-				<li>Do you have <b>{$this->category_slug}</b> post type posts? You can check here: <a target='_blank' href='$dynamic_url'>{$dynamic_url}</a>
+				<li>Do you have <b>{$this->taxonomy}</b> post type posts? You can check here: <a target='_blank' href='$dynamic_url'>{$dynamic_url}</a>
 			</ol>
 		</div>
 		";
@@ -47,22 +47,38 @@ class Categories_To_migrate extends Requirement {
 
 	public function title() {
 
-		$count = $this->get_terms();
+		$count = $this->get_term_count();
+		if ( $this->requirement_is_met ) {
+			return "Existing portfolio category count: <b>{$count}</b> ";
+		}
 
-		return "Existing portfolio category count: <b>{$count}</b> ";
+		if ( ! taxonomy_exists( $this->taxonomy ) ) {
+			return "Taxonomy <b>{$this->taxonomy}</b> not found!";
+		}
+
+		if ( $this->get_term_count() === 0 ) {
+			return "Portfolio category count is <b>0</b>";
+		}
+
+
 	}
 
 
 	public function check() {
 
-		return $this->get_terms() > 0;
+
+		return (
+			taxonomy_exists( $this->taxonomy )
+			&&
+			$this->get_term_count() > 0
+		);
 	}
 
 
-	public function get_terms() {
+	public function get_term_count() {
 
 		$terms = get_terms(
-			$this->category_slug,
+			$this->taxonomy,
 			[
 				'hide_empty' => false,
 			]
